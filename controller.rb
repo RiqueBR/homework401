91967bf22270b2209a8f9aefd0dece4fc1db50ca
Henrique Pereira Batista <h.p.batista20@gmail.com>
2018-09-24 19:41:50 +0100

    worked on extensions

--- controller.rb
+++ controller.rb
@@ -5,6 +5,18 @@ require('pry-byebug')
 require_relative('./models/game')
 also_reload('./models/*')
 
+get '/welcome_page' do
+  erb(:welcome)
+end
+
+get '/result' do
+  erb(:result)
+end
+
+get '/game_rules' do
+  erb(:game_rules)
+end
+
 get '/game/:player1/:player2' do
   game = Game.new(params[:player1], params[:player2])
   @simple_game = game.check_winner()
